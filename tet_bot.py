cf9ec5a0844ba2934a449885d5380906fadeb389
Piotr Jankowski <118063741+pj-system@users.noreply.github.com>
2023-07-31 23:32:06 +0100

    Create tet_bot.py
    foundation for the bot class

---
+++ tet_bot.py
@@ -0,0 +1,12 @@
+class TetBot():
+    def __init__(self, tet_game) -> None:
+        pass
+
+    def generate_moves(self):
+        pass
+
+    def calculate_move(self):
+        pass
+
+    def make_move(self):
+        pass
